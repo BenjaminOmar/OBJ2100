10e00edc3ddebcece42f95cbacfe135bcc1a5d1b
ellenkh <92035794+ellenkh@users.noreply.github.com>
2023-05-27 12:45:43 +0200

    test

--- Metods/src/Methods/UserInterface.java
+++ Metods/src/Methods/UserInterface.java
@@ -1,23 +1,5 @@
 package Methods;
 
-import javax.swing.*;
-
-
-
 public class UserInterface {
-	
-	public static void main(String[] args) {
-		
-	}
-	
-	public static void jFrame1() {
-		//create a JFrame so you can place other objects within it for display
-		
-		JFrame aFrame = new JFrame("First Frame");
-	}
-	
-	public static void test() {
-		
-	}
 
 }
