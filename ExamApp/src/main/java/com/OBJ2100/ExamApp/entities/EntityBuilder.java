aa9e7f64e1cf85d7f9872fe4be28e052fcd7d1c8
antonindejesus <antonin.dejesus@outlook.com>
2023-05-30 00:33:30 +0200

    Add EntityBuilder comments

--- ExamApp/src/main/java/com/OBJ2100/ExamApp/entities/EntityBuilder.java
+++ ExamApp/src/main/java/com/OBJ2100/ExamApp/entities/EntityBuilder.java
@@ -1,10 +1,19 @@
 package com.OBJ2100.ExamApp.entities;
 
 
-
 public interface EntityBuilder<T> {
-	
+
+	/**
+	 * Returns the entity being built.
+	 * 
+	 * @return an instance of the specific entity
+	 */
 	T build();
 	
+	/**
+	 * Checks whether values used when building are valid.
+	 * 
+	 * @throws IllegalStateException
+	 */
 	void validate() throws IllegalStateException;
 }
