bf95666e7a621aaaf919ab6480d39669ac4abad3
ellenkh <92035794+ellenkh@users.noreply.github.com>
2023-05-30 22:16:32 +0200

    FileAccessSettingsPanel and ListCustomers panel.

--- ExamApp/src/main/java/com/OBJ2100/ExamApp/gui/FileAccessSettingsPanel.java
+++ ExamApp/src/main/java/com/OBJ2100/ExamApp/gui/FileAccessSettingsPanel.java
@@ -2,58 +2,79 @@ package com.OBJ2100.ExamApp.gui;
 
 import javax.swing.JFrame;
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout; 
 import javax.swing.JLabel;
+import java.awt.FlowLayout;
 
-import com.OBJ2100.ExamApp.documents.FolderManager;
-public class FileAccessSettingsPanel extends JPanel  {
-	
-	private final JButton changeFolderBtn; 
-	private final JLabel folderPath; 
-		
+import com.OBJ2100.ExamApp.gui.Listeners.ChangeFolderListener;
+
+/**
+ * A panel that displays file access settings.
+ * @author 7162
+ */
+
+
+public class FileAccessSettingsPanel extends JPanel {
+
+	private final JButton changeFolderBtn; // Button to change the folder
+	private final JLabel folderPath; // Label to display the selected folder path
+
+	/**
+	 * Constructs a FileAccessSettingsPanel.
+	 */
 	public FileAccessSettingsPanel() {
 
-		//her må det legges til en metode som viser "..." når filbanen blir lengre enn 
-		// bredden på JLabel. Det er det enklest å gjøre der man kaller på komponentet og man har bredden på elementet. 
-		setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
-		setPreferredSize(new Dimension(600, 75));
-		setBorder(BorderFactory.createTitledBorder("File Access Settings"));
+		setPreferredSize(new Dimension(600, 60));
 		setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "File Access Settings"));
-	
+
 		int paddingSize = 5;
-        setBorder(BorderFactory.createCompoundBorder(
-                getBorder(),
-                BorderFactory.createEmptyBorder(paddingSize, paddingSize, paddingSize, paddingSize)
-		));
-		changeFolderBtn = new JButton("Change Folder");	
-		changeFolderBtn.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String selectedFolder = FolderManager.chooseFolder();
-				if (selectedFolder != null){
-					folderPath.setText("  Current folder: " + selectedFolder);
-				}						
-			}		
-		});
+		setBorder(BorderFactory.createCompoundBorder(
+				getBorder(),
+				BorderFactory.createEmptyBorder(paddingSize, paddingSize, paddingSize, paddingSize)));
+		setLayout(new FlowLayout(FlowLayout.LEFT, 10, 0));
+		changeFolderBtn = new JButton("Change Folder");
+		changeFolderBtn.addActionListener(new ChangeFolderListener(this));
 		add(changeFolderBtn);
-		
-		folderPath = new JLabel("  No folder is selected");	
-		add(folderPath); 	
+
+		folderPath = new JLabel("  No folder is selected");
+		add(folderPath);
 	}
 
-	
-	public static void main(String[] args) { 
-		
-		JFrame frame = new JFrame("FileAccessPanel");
-		frame.getContentPane().add( new FileAccessSettingsPanel());
+	/**
+	 * Returns the label component for the folder path.
+	 *
+	 * @return the folder path label
+	 */
+
+	public JLabel getFolderPath() {
+		return folderPath;
+	}
+
+	/**
+	 * Checks if the text in folderPath is longer than the specified number of
+	 * characters. If the text is too long, it is truncated and replaced with dots
+	 * ("...") at the end to indicate that there is more text that is not displayed.
+	 */
+	public void checkTextLength() {
+		String text = folderPath.getText();
+		int maxLength = 50; // Maximum number of characters allowed
+		int textLength = text.length();
+
+		if (textLength > maxLength) {
+			String truncatedText = text.substring(0, maxLength - 3) + "...";
+			folderPath.setText(truncatedText);
+		}
+	}
+
+	public static void main(String[] args) {
+		com.formdev.flatlaf.FlatLightLaf.setup();
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(new FileAccessSettingsPanel());
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
+
 	}
-}	
+}
--- ExamApp/src/main/java/com/OBJ2100/ExamApp/gui/ListCustomersPanel.java
+++ ExamApp/src/main/java/com/OBJ2100/ExamApp/gui/ListCustomersPanel.java
@@ -1,5 +1,83 @@
 package com.OBJ2100.ExamApp.gui;
 
-public class ListCustomersPanel {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class ListCustomersPanel extends JPanel {
+
+    private final JRadioButton byCityRadioButton;
+    private final JLabel byCityLabel;
+    private final JComboBox<String> dropdownCity;
+    private final JRadioButton byStateRadioButton;
+    private final JLabel byStateLabel;
+    private final JComboBox<String> dropdownState;
+    private final JButton writeToFileButton;
+
+    public ListCustomersPanel() {
+        setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "List Customers"));
+        setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(0, 0, 10, 10);
+
+        byCityRadioButton = new JRadioButton("City");
+        add(byCityRadioButton, gbc);
+
+        gbc.gridx = 1;
+        byCityLabel = new JLabel("Label City:");
+        add(byCityLabel, gbc);
+
+        gbc.gridx = 2;
+        gbc.weightx = 1.0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        dropdownCity = new JComboBox<>();
+        add(dropdownCity, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.weightx = 0.0;
+        gbc.fill = GridBagConstraints.NONE;
+        byStateRadioButton = new JRadioButton("State");
+        add(byStateRadioButton, gbc);
+
+        gbc.gridx = 1;
+        byStateLabel = new JLabel("Label State:");
+        add(byStateLabel, gbc);
+
+        gbc.gridx = 2;
+        gbc.weightx = 1.0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        dropdownState = new JComboBox<>();
+        add(dropdownState, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 3;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.insets = new Insets(20, 0, 0, 0);
+        writeToFileButton = new JButton("Write customers list that match selected criteria into file...");
+        add(writeToFileButton, gbc);
+
+        // Set up action listener for the button
+        writeToFileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Handle button click event
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("List Customers Panel");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new ListCustomersPanel());
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
