ebc72dcc67c9353a723970bea61749018784e9cb
Benjamin <92789367+BenjaminOmar@users.noreply.github.com>
2023-05-31 12:27:40 +0200

    Comittet siste endringer for sqlQuery

--- ExamApp/src/main/java/com/OBJ2100/ExamApp/gui/Helpers/MessageHelper.java
+++ ExamApp/src/main/java/com/OBJ2100/ExamApp/gui/Helpers/MessageHelper.java
@@ -1,17 +1,44 @@
 package com.OBJ2100.ExamApp.gui.Helpers;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Font;
 
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
+import javax.swing.JTextArea;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class MessageHelper {
 	
 	private static Font chosenFont = new Font("Calibri", Font.PLAIN, 15);
+	private static JScrollPane scroll;
+	private static JTextArea textArea;
+	private static JPanel panel;
+	
 	
 	public static void displayMessage(String message) {
 		UIManager.put("OptionPane.messageFont", chosenFont);
 		UIManager.put("OptionPane.buttonFont", chosenFont);
 		JOptionPane.showMessageDialog( null, message, "Database connection", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	public static void displayMessageError(Object printStackTrace) {
+        textArea = new JTextArea(String.valueOf(printStackTrace));
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setBorder(null);
+
+        scroll = new JScrollPane(textArea);
+        scroll.setPreferredSize(new Dimension(400, 200));
+
+        panel = new JPanel(new BorderLayout());
+        panel.add(scroll, BorderLayout.CENTER);
+
+        UIManager.put("OptionPane.messageFont", chosenFont);
+        UIManager.put("OptionPane.buttonFont", chosenFont);
+
+        JOptionPane.showMessageDialog(null, panel, "Error", JOptionPane.PLAIN_MESSAGE);
 	}
 }
--- ExamApp/src/main/java/com/OBJ2100/ExamApp/gui/Listeners/ExecSqlQueryListener.java
+++ ExamApp/src/main/java/com/OBJ2100/ExamApp/gui/Listeners/ExecSqlQueryListener.java
@@ -1,38 +1,53 @@
 package com.OBJ2100.ExamApp.gui.Listeners;
 
-
 import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.sql.ResultSetMetaData;
 import java.awt.FlowLayout;
-import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.Console;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
+
+import org.omg.CORBA.PRIVATE_MEMBER;
+
+import com.OBJ2100.ExamApp.db.DataSourceFactory;
+import com.OBJ2100.ExamApp.gui.SideMenu;
+import com.OBJ2100.ExamApp.gui.Helpers.MessageHelper;
+import com.mysql.cj.protocol.Resultset;
+
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.sql.DataSource;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 
 
 public class ExecSqlQueryListener implements ActionListener{
+	JLabel headerLabel = new JLabel("Write your SQL statement:");
     JPanel panel = new JPanel(new BorderLayout());
     JTextArea textArea = new JTextArea();
     JDialog dialog = new JDialog();
     JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
     JButton button = new JButton("Execute");
+    private String query;
 	
     @Override
 	public void actionPerformed(ActionEvent e) {
-		performSqlQuery();	
+		SQLPopup();	
 	}
 	
-	private void performSqlQuery() {
-
-
+	private void SQLPopup() {
+		panel.add(headerLabel, BorderLayout.NORTH);
+		headerLabel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 	    buttonPanel.add(button);
+	    
 
 	    panel.add(new JScrollPane(textArea), BorderLayout.CENTER);
 	    panel.add(buttonPanel, BorderLayout.SOUTH);
@@ -45,10 +60,44 @@ public class ExecSqlQueryListener implements ActionListener{
 
 	    button.addActionListener(new ActionListener() {
 	        public void actionPerformed(ActionEvent e) {
-	            System.out.println(textArea.getText().trim());
+	            query = textArea.getText().trim();
+	           
+	            executeSQLQuery(query);
+	    		
 	            dialog.dispose(); // Close the dialog
 	        }
 	    });
 	}
+	private void executeSQLQuery(String query) {
+  		DataSource source = DataSourceFactory.getMySqlDataSource();
+		
+  		try (Connection connection = source.getConnection()) {
+  			Statement statement = connection.createStatement();
+			
+			if (query.contains("SELECT")) {
+				ResultSet rs = statement.executeQuery(query);	    				
+				
+				ResultSetMetaData Metadata = rs.getMetaData();
+				
+				while (rs.next()) {
+					for (int i = 1; i < rs.getMetaData().getColumnCount() + 1; i++) {
+			              System.out.print(rs.getObject(i) + ",");
+					}
+				}
+				
+				statement.close();
+			} else {
+				int totalOfRows =  statement.executeUpdate(query);
+				MessageHelper.displayMessage("SQL statement executed successfully! "
+						+ "\n\n total rows affected: " + totalOfRows);
+				statement.close();
+			}
+		} catch (SQLException err) {
+			MessageHelper.displayMessageError(err);
+			
+		}
+	}
+	
+	
 
 }
