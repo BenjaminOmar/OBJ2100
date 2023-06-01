21e08c6b5a1f0cbade7a038fc530625fae55ff95
antonindejesus <antonin.dejesus@outlook.com>
2023-06-01 16:25:31 +0200

    Add missing customers table dialog

---
+++ ExamApp/src/main/java/com/OBJ2100/ExamApp/gui/dialogs/ListAllProductsTableDialog.java
@@ -0,0 +1,30 @@
+package com.OBJ2100.ExamApp.gui.dialogs;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.OBJ2100.ExamApp.db.dao.factories.DaoFactory;
+import com.OBJ2100.ExamApp.entities.Product;
+
+public class ListAllProductsTableDialog extends AbstractListAllTableDialog<Product> {
+
+	@Override
+	protected List<Product> fetchEntities(DaoFactory daoFactory) throws SQLException {
+		return daoFactory.getProductDao().getAll();
+	}
+	
+	@Override
+	protected Object[] extractOne(Product p) {
+		return new Object[] {
+				p.getProductCode(),
+				p.getProductName(),
+				p.getProductLine(),
+				p.getProductScale(),
+				p.getProductVendor(),
+				p.getProductDescription(),
+				p.getQuantityInStock(),
+				p.getBuyPrice(),
+				p.getMsrp()
+		};
+	}
+}
